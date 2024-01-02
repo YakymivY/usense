d228ada89f85fa59e5701e5344f6445159d78478
Yuriy Yakymiv <yuriyyakymiv@Yuriys-MacBook-Air.local>
2024-01-03 00:00:17 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'usense';
+
+  passwordForm: FormGroup;
+  isShort: boolean = false;
+  charTypes: number = 0;
+
+  constructor (private fb: FormBuilder) {
+    //initializing the form
+    this.passwordForm = this.fb.group({
+      password: ['']
+    });
+  }
+
+  //password getter
+  get passwordValue() {
+    return this.passwordForm.get('password')?.value;
+  }
+
+  checkStrength(): void {
+    //clearing values
+    this.isShort = false;
+    this.charTypes = 0;
+
+    //matching conditions
+    if (this.passwordValue.length < 8 && this.passwordValue.length != 0) this.isShort = true;
+    else this.charTypes = this.checkContent(this.passwordValue);
+  }
+
+  checkContent(str: string): number {
+    let count: number = 0;
+    //string has letters
+    if (/[A-Za-z]/.test(str)) {
+      count++;
+    }
+    //string has digits
+    if (/\d/.test(str)) {
+      count++;
+    }
+    //string has symbols
+    if (/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(str)) {
+      count++;
+    }
+    return count;
+  }
+
 }
